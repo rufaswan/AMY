aafe345b2b9b21886c1e2b46674d7427ded18ab5
Rufas Wan <rufaswan@gmail.com>
2018-07-19 18:36:25 +0800

    git-update last

--- git-update.sh
+++ git-update.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
 git="git@github.com:rufaswan/CX.git"
+ups=""
 
 msg="
-please type a comment to push
-  type pull  to do a git pull
-  type force to overwrite the repo
+please type a comment to commit and push
+  type -retry to retry git push [no commit]
+  type -pull  to do a git pull
+  type -fetch to sync your forked repo
+  type -force to overwrite the repo
 "
 [ $# = 0 ] && { echo "$msg"; exit; }
 
 git remote rm  origin
 git remote add origin "$git"
+git remote rm  upstream
+git remote add upstream "$ups"
 
 case "$1" in
 	"-pull")
@@ -21,6 +26,23 @@ case "$1" in
 		[[ "$2" == "i_really_want_to_do_this" ]] || exit
 		echo "git push --force $git : master"
 		git push --force origin master
+		;;
+	"-retry")
+		echo "git push $git"
+		git push origin master
+		;;
+	"-update")
+		if [ "$ups" ]; then
+			echo "git fetch $ups"
+			git fetch upstream
+			git checkout master
+			git merge upstream/master
+		else
+			echo "No upstream defined. Nothing to fetch."
+		fi
+		;;
+	"-last")
+		git log --pretty=oneline -5
 		;;
 	*)
 		echo "git push $git : $@"
